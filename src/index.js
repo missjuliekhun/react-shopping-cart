a1cca639920e64951495aca33541c674308b5cbd
missjuliekhun <jhuy.khun@gmail.com>
2017-01-31 15:43:45 -0500

    started/attempted newItem component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import addMore from './newItem';
 
 class ShoppingCart extends React.Component {
   render(){
---
+++ src/newItem.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class newItem extends Component{
+    render()
+      return(
+        <h1>Add More</h1>
+      )
+
+
+  }
+
+
+  export default newItem
